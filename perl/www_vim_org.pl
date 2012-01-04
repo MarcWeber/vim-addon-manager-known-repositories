4dbecaf377f4e76f55ac592164c08671f6d005d7
ZyX <kp-pav@yandex.ru>
2012-01-04 22:03:10 +0400

    Purged old HTML parsing stuff

--- perl/www_vim_org.pl
+++ perl/www_vim_org.pl
@@ -9,7 +9,6 @@ use Fcntl qw(:DEFAULT :flock);
 use Time::HiRes;
 use utf8;
 use Data::Dumper;
-use YAML::XS;
 use JSON;
 use HTML::Entities;
 
@@ -44,133 +43,6 @@ sub get($) {
     }
     die "Failed to get $url";
 }
-#▶1 getHTML :: response → tree
-sub getHTML($) {
-    my $response=shift;
-    my $tree=HTML::TreeBuilder->new();
-    $tree->parse($response->decoded_content) or
-        die "$!\nFailed to parse ".$response->decoded_content;
-    return $tree;
-}
-#▶1 getScripts :: [ urlargs] → [script]
-# This will give us a list of all scripts
-sub getScripts(;$);
-sub getScripts(;$) {
-    my $urlargs=shift || "";
-    my $url="$base/script_search_results.php$urlargs";
-    print "Processing $url\n" if($verbose);
-    my $response=get($url);
-    my $tree=getHTML($response);
-    my $table=$tree->look_down(_tag => 'table',
-                               sub {
-                                   my $t=$_[0];
-                                   (scalar ($t->content_list) > 2) or return 0;
-                                   $t=[$t->content_list]->[0];
-                                   return ($t->as_text =~ /Searched scripts/);
-                               });
-    die "Failed to find table on $url" unless $table;
-    my @scripts;
-    for my $row (splice @{[$table->content_list]}, 2, -1) {
-        next unless ref $row;
-        my @fields=($row->content_list);
-        next unless (($fields[0]->attr('class') || "") =~ /^row/);
-        my $a=[$fields[0]->content_list]->[0];
-        my $snr;
-        $snr=+$1 if ($a->attr('href') =~ /(\d+)$/);
-        next unless $snr;
-        push @scripts, {snr => $snr,
-                       name => $a->as_text,
-                       type => $fields[1]->as_text,
-                     rating => +($fields[2]->as_text),
-                  downloads => +($fields[3]->as_text),
-                description => $fields[4]->as_text};
-    }
-    my $l=[[$table->content_list]->[-2]->content_list]->[0]->look_down(
-                    _tag => 'a',
-                    sub { $_[0]->as_text =~ /next/i });
-    if($l) {
-        my $link=$l->attr('href');
-        $link=~s/.*\?&(.*)/?$1/;
-        push @scripts, getScripts($link);
-    }
-    return @scripts;
-}
-#▶1 processScript :: script → + script
-sub processScript($) {
-    my $script  = shift;
-    my $url="$base/script.php?script_id=".$script->{"snr"};
-    print "Processing $url\n" if($verbose);
-    my $response=get($url);
-    my $tree=getHTML($response);
-    my $ktr=[$tree->look_down(_tag => 'table',
-                              sub {
-                                  my $t=$_[0];
-                                  (scalar ($t->content_list)==2) or return 0;
-                                  $t=[$t->content_list]->[0];
-                                  return ($t->as_text =~ /Downloaded by \d+/);
-                              })->content_list]->[0];
-    if($ktr->as_text =~ /Rating (-?\d+)\/(\d+)/) {
-        $script->{"rating_full"}=[(+$1), (+$2)];
-    }
-    my $table=$tree->look_down(_tag => 'table',
-                               sub {
-                                   my $t=$_[0];
-                                   (scalar ($t->content_list) > 9) or return 0;
-                                   $t=[$t->content_list]->[0];
-                                   return ($t->as_text =~ /created by/);
-                               });
-    my $state;
-    for my $line ($table->content_list) {
-        next unless ref $line;
-        my $fel=[$line->content_list]->[0];
-        if($fel->attr('class')) {
-            my $text=[$fel->content_list]->[0];
-            if(not ref $text) {
-                if($text =~ /created by/) {
-                    $state='author'; }
-                elsif($text =~ /description/) {
-                    $state='description'; }
-                elsif($text =~ /install details/) {
-                    $state='install'; }
-            }
-            next;
-        }
-        if($state eq 'author') {
-            $script->{"author"}=$fel->as_text; }
-        elsif($state) {
-            $script->{$state}=$fel->as_HTML('<>&', "\t", {}); }
-        $state="";
-    }
-    $table=$tree->look_down(_tag => 'table',
-                            sub {
-                                my $t=$_[0];
-                                (scalar ($t->content_list) > 1) or return 0;
-                                $t=[$t->content_list]->[0];
-                                $t->attr('class') or return 0;
-                                (scalar ($t->content_list)==7) or return 0;
-                                $t=[$t->content_list]->[0];
-                                ref [$t->content_list]->[0] and return 0;
-                                $t=[$t->content_list]->[0];
-                                return ($t =~ /package/);
-                            });
-    $script->{"sources"}=[];
-    for my $row (splice @{[$table->content_list]}, 1) {
-        next unless ref $row;
-        my @fields=($row->content_list);
-        next unless (($fields[0]->attr('class') || "") =~ /^row/);
-        my $a=[$fields[0]->content_list]->[0];
-        my $srcnr;
-        $srcnr=+$1 if ($a->attr('href') =~ /(\d+)$/);
-        push @{$script->{"sources"}},
-                  {srcnr => +$srcnr,
-                 archive => $a->as_text,
-                 version => ($fields[1]->as_text),
-                    date => ($fields[2]->as_text),
-             vim_version => ($fields[3]->as_text),
-             description => ($fields[5]->as_HTML('<&>', "\t", {})),
-                  };
-    }
-}
 #▶1 WL :: filehandle, line → + FS
 sub WL($$) {
     my $F=shift;
@@ -202,29 +74,11 @@ sub formatKey($$) {
     $r.=", ";
     return $r;
 }
-#▶1 formatScripts :: fh, fh, [script] → + FS: scripts.yaml, scripts.dat
-sub formatScripts($$$;$) {
-    my $VIM     = shift;
-    my $YAML    = shift;
-    my $scripts = shift;
-    my $nodl    = defined shift;
+#▶1 formatScripts :: [script], fh, fh, fh → + FS: scripts.yaml, scripts.dat, db/
+sub formatScripts($@) {
+    my $scripts=shift;
+    my ($VIM, $NrNDB, $NNrDB) = @_;
     for my $script (@$scripts) {
-        unless($nodl) {
-            eval {processScript($script);};
-            if($@) {
-                print STDERR "Failed to process script ".$script->{"snr"}.": $@";
-                next;
-            }
-            elsif(not ref $script->{"sources"}) {
-                print STDERR "sources is not a reference:\n";
-                print STDERR Data::Dumper->Dump([$script], ['script']);
-                next;
-            }
-            elsif(not scalar @{$script->{"sources"}}) {
-                print STDERR "No sources for script ".$script->{"snr"}."\n";
-                next;
-            }
-        }
         my $lastsrc=$script->{"sources"}->[0];
         my $line="let s:p";
         if($script->{"id"} =~ /^[a-zA-Z0-9_]+$/) {
@@ -240,13 +94,10 @@ sub formatScripts($$$;$) {
                     "'vim_script_nr': ".$script->{"snr"}.", ".
                     "'type': 'archive'}\n";
         WL($VIM, $line);
-        if($dumpall) {
-            WL($YAML, YAML::XS::Dump($script));
-        }
     }
 }
-#▶1 openScript :: () → FD + …
-sub openScript() {
+#▶1 openDBs :: () → FD + …
+sub openDBs() {
     my $VIM;
     open $VIM, '>:utf8', $vimtarget
         or die $!;
@@ -257,7 +108,7 @@ sub openScript() {
     print $VIM "    let g:vim_addon_manager.vim_org_sources={}\n";
     print $VIM "endif\n";
     print $VIM "let s:p=g:vim_addon_manager.vim_org_sources\n";
-    return $VIM;
+    return ($VIM, undef, undef);
 }
 #▶1 genName :: name, snr, scriptnames → sname + scriptnames
 sub genName($$$) {
@@ -290,75 +141,15 @@ sub addScriptID($) {
         $scriptnames->{$script->{"id"}}=$script;
     }
 }
-#▶1 getAllScripts_parseHTML :: () → + …
-sub getAllScripts_parseHTML() {
-    local $_;
-    my $scripts=[getScripts("?show_me=4000")];
-    addScriptID($scripts);
-    my $numscripts=((scalar @$scripts)/$threads);
-    if(($threads*$numscripts)<(scalar @$scripts)) {
-        $threads++; }
-    my $i=0;
-    while($i<$threads) {
-        my $pid=fork;
-        if($pid) {
-            $children{$pid}=1;
-            $i++;
-        }
-        else {
-            my $VIM;
-            open $VIM, '>:utf8', "$vimtarget.$i"
-                or die $!;
-            my $YAML;
-            if($dumpall) {
-                open $YAML, '>:utf8', "$yamltarget.$i"
-                    or die $!; }
-            formatScripts($VIM, $YAML,
-                [@$scripts[($i*$numscripts)..((($i+1)*$numscripts)-1)]]);
-            return;
-        }
-    }
-    map {waitpid $_, 0} (keys %children);
-    my $YAML;
-    if($dumpall) {
-        open $YAML, '>:utf8', $yamltarget
-            or die $!; }
-    my $VIM=openScript();
-    $i=0;
-    while($i<$threads) {
-        my $VIM2;
-        open $VIM2, '<:utf8', "$vimtarget.$i"
-            or die $!;
-        while(<$VIM2>) {
-            print $VIM $_; }
-        close $VIM2;
-        unlink "$vimtarget.$i";
-        if($dumpall) {
-            my $YAML2;
-            open $YAML2, '<:utf8', "$yamltarget.$i"
-                or die $!;
-            while(<$YAML2>) {
-                print $YAML $_; }
-            close $YAML2;
-            unlink "$yamltarget.$i";
-        }
-        $i++;
-    }
-    close $VIM;
-    close $YAML if($dumpall);
-}
 #▶1 getAllScripts
 sub getAllScripts() {
-    # return getAllScripts_parseHTML();
     my $url="$vimorg/script-info.php";
     print "Processing $url\n" if($verbose);
     my $response=get($url);
-    return getAllScripts_parseHTML() unless $response;
     my $json;
     eval {$json=JSON::decode_json($response->decoded_content())};
     unless(defined $json) {
-        print STDERR "Failed to parse json: $@";
-        return getAllScripts_parseHTML();
+        die "Failed to parse json: $@";
     }
     local $_;
     my $scripts=[sort {$b->{"snr"} <=> $a->{"snr"}}
@@ -374,8 +165,8 @@ sub getAllScripts() {
                                                @{$_->{"releases"}})],
                             }} values %$json)];
     addScriptID($scripts);
-    my $VIM=openScript();
-    formatScripts($VIM, undef, $scripts, 0);
+    formatScripts($scripts, openDBs());
     return;
 }
 getAllScripts();
+# vim: ft=perl tw=80 ts=4 sts=4 sw=4 et fmr=▶,▲
