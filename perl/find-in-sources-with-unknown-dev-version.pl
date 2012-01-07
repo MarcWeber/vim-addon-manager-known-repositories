d26599a0d028219beaa817ffa9ccb5f67c5939e1
ZyX <kp-pav@yandex.ru>
2012-01-07 19:48:30 +0400

    Added scripts that help to populate SCM database

---
+++ perl/find-in-sources-with-unknown-dev-version.pl
@@ -0,0 +1,84 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use JSON;
+use LWP::Simple;
+
+if($ARGV[0] eq "--help") {
+    print "Usage: $0 regex\n";
+    print "You must run this with current directory set to vam-kr root.\n";
+    print "Do not forget to run\n";
+    print "    curl 'http://www.vim.org/script-info.php' > vodb.json\n";
+    print "before running this script.\n";
+}
+
+binmode STDOUT, ':utf8';
+
+my $scmdb="db/scmsources.vim";
+my $vimorg="http://www.vim.org";
+my $vodburl="$vimorg/script-info.php";
+
+my $regex=shift @ARGV;
+$regex=qr($regex);
+
+my $sumreg=$regex;
+my $descreg=$regex;
+my $instreg=$regex;
+
+my $VIM;
+open $VIM, '<:utf8', $scmdb;
+
+my %knownsources=();
+local $_;
+while(<$VIM>) {
+    next unless /^(?:" )?let scmnr\.(\d+)\s*=/;
+    $knownsources{$1}=$_;
+}
+close $VIM;
+
+# my %vo=%{JSON::decode_json(LWP::Simple::get($vodburl))};
+my $VODB;
+open $VODB, '<:utf8', "vodb.json";
+my %vo=%{JSON::decode_json(join "", <$VODB>)};
+close $VODB;
+sub PrintWithPrefix {
+    my ($prefix, $text)=@_;
+    print $prefix;
+    $prefix=~s/./ /g;
+    $text=~s/\r//g;
+    local $_;
+    my @lines=split /\n/, $text;
+    print (shift @lines);
+    print "\n";
+    for my $line (map {"$prefix$_"} @lines) {
+        print "$line\n";
+    }
+}
+sub PrintSinfo {
+    my ($sinfo)=@_;
+    print "Script type: ".$sinfo->{"script_type"}."\n";
+    print "Author: ";
+    print $sinfo->{"first_name"} if($sinfo->{"first_name"});
+    print " " if($sinfo->{"first_name"} and $sinfo->{"last_name"});
+    print $sinfo->{"last_name"} if($sinfo->{"last_name"});
+    print "\n";
+    print "Summary: ".$sinfo->{"summary"}."\n";
+    PrintWithPrefix("Description: ", $sinfo->{"description"});
+    PrintWithPrefix("Install details: ", $sinfo->{"install_details"});
+}
+for my $snr (sort {$a<=>$b} (grep {not defined $knownsources{$_}} (keys %vo))) {
+    my $sinfo=$vo{$snr};
+    if(defined $sumreg and $sinfo->{"summary"} =~ $sumreg) {
+        print "Summary matches for $snr:\n";
+        PrintSinfo($sinfo);
+    }
+    elsif(defined $descreg and $sinfo->{"description"} =~ $descreg) {
+        print "\nDescription matches for $snr:\n";
+        PrintSinfo($sinfo);
+    }
+    elsif(defined $instreg and $sinfo->{"install_details"} =~ $instreg) {
+        print "Installation details match for $snr:\n";
+        PrintSinfo($sinfo);
+    }
+}
---
+++ tools/scmdbhelpers.vim
@@ -0,0 +1,47 @@
+function! GetAuthor()
+    wincmd k
+    let view=winsaveview()
+    .+1? for \d\+:$?+2
+    let a=getline('.')
+    call winrestview(view)
+    wincmd p
+    return matchstr(a, 'Author: \zs.*')
+endfunction
+function! GetScriptType()
+    wincmd k
+    let view=winsaveview()
+    .+1? for \d\+:$?+1
+    let a=getline('.')
+    call winrestview(view)
+    wincmd p
+    return matchstr(a, 'Script type: \zs.*')
+endfunction
+fun! AddGHUrl(url, nr)
+    let nr=a:nr
+    if nr is 0 | let nr=substitute(matchstr(a:url, '\v^(\w+\:\/\/)?[^/]+\/[^/]+\/\zs[^/]+'), '\v(\.vim)?(\.git)?/*$', '', '') | endif
+    call append('.', ((type(nr)==type(0))?
+                \       ('let scmnr.'.nr):
+                \       ('let scm['.string(nr).']')).' = '.
+                \    ((a:url=~#'\v\/(blob|raw)\/master\/.*\.vim$')?
+                \       ('{''url'': '.string(substitute(a:url, 'blob/master', 'raw/master', '')).', ''archive_name'': '.string(fnamemodify(a:url, ':t')).', ''type'': ''archive'', ''script-type'': '.string(GetScriptType()).'}'):
+                \    ((a:url=~#'^lp:')?
+                \       ('{''type'': ''bzr'', ''url'': '.string(a:url).'}'):
+                \    ((a:url=~#'svn')?
+                \       ('{''type'': ''svn'', ''url'': '.string(a:url).'}'):
+                \    ((a:url=~#'bitbucket\.org')?
+                \       ('{''type'': ''hg'', ''url'': '.string(a:url).'}'):
+                \       ('{''type'': ''git'', ''url'': ''git://'.substitute(substitute(substitute(substitute(a:url, '^\w\+://', '', ''), '/\(/\|$\)\@=', '', ''), '^\([^/]*/[^/]*/[^/]*\).*', '\1', ''), '\.git$', '', '').'''}'))))))
+endfunction
+function! GetSNR()
+    wincmd k
+    let view=winsaveview()
+    .+1? for \d\+:$?
+    let a=getline('.')
+    call winrestview(view)
+    wincmd p
+    return matchstr(a, '\d\+\ze:$')
+endfunction
+nnoremap ,gv :call AddGHUrl(@+, +GetSNR())<CR>j
+nnoremap ,gg :call AddGHUrl(@+, 0)<CR>j
+nnoremap ,ge :call AddGHUrl(@+, )<Left>
+nnoremap ,ga :call append('.', ['', '" '.GetAuthor()])<CR>2j
