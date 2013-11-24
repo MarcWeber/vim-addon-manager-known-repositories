0c25fd9786bb8f8d0564d4c5b5c24a5e691bda24
ZyX <kp-pav@ya.ru>
2013-11-24 23:38:53 +0400

    Add only_trunk argument controlling what list_svn_files lists

--- python/list_svn_files.py
+++ python/list_svn_files.py
@@ -7,8 +7,10 @@ from subvertpy import NODE_DIR
 class NoFilesError(Exception):
     pass
 
-def _list_svn_files(conn, path=''):
+def _list_svn_files(conn, path='', only_trunk=True):
     dirents, frev, props = conn.get_dir(path, -1, DIRENT_KIND)
+    if not path and only_trunk and 'trunk' in dirents:
+        dirents = {'trunk' : dirents['trunk']}
     for fname, props in dirents.items():
         new_path = (path + '/' if path else '') + fname
         contains = False
@@ -18,14 +20,16 @@ def _list_svn_files(conn, path=''):
         else:
             yield new_path
 
-def list_svn_files(url):
+def list_svn_files(url, only_trunk=True):
     '''List files in remote subversion repository
 
     Returns either an empty list or a generator.
+
+    If only_trunk is True, then if trunk/ directory is present at the top level, only it is used.
     '''
     conn = RemoteAccess(url)
     try:
-        return _list_svn_files(conn)
+        return _list_svn_files(conn, only_trunk=only_trunk)
     except NoFilesError:
         return []
 
