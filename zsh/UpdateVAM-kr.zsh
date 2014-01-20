fa059149d26ce4e28a1b2faf0f2927c8af95a578
ZyX <kp-pav@ya.ru>
2014-01-20 20:52:12 +0400

    Make it clear that VAM-kr is now a mirror of vim-pi

--- zsh/UpdateVAM-kr.zsh
+++ zsh/UpdateVAM-kr.zsh
@@ -1,8 +1,9 @@
 #!/bin/zsh
 set -x
+VIMPIREP=ssh://git@bitbucket.org/vimcommunity/vim-pi
 VAMKRREP=ssh://git@github.com/MarcWeber/vim-addon-manager-known-repositories
-VAMKRDIR=~/tmp/vam-kr
-VAMKR_SAFE_REPO=~/.vam/vim-addon-manager-known-repositories
+VIMPIDIR=~/tmp/vim-pi
+VIMPI_SAFE_REPO=~/.vam/vim-pi
 
 GIT=/usr/bin/git
 PYTHON=/usr/bin/python
@@ -10,29 +11,48 @@ PERL=/usr/bin/perl
 CURL=/usr/bin/curl
 RM=/bin/rm
 
-if ! test -d $VAMKRDIR ; then
-    test -d $VAMKRDIR:h || /bin/mkdir -p $VAMKRDIR:h || exit 1
-    $GIT clone $VAMKRREP $VAMKRDIR || exit 2
-    cd $VAMKRDIR || exit 3
+if ! test -d $VIMPIDIR ; then
+    test -d $VIMPIDIR:h || /bin/mkdir -p $VIMPIDIR:h || exit 1
+    $GIT clone $VIMPIREP $VIMPIDIR || exit 2
+    cd $VIMPIDIR || exit 3
 else
-    cd $VAMKRDIR || exit 3
+    cd $VIMPIDIR || exit 3
     $GIT pull
 fi
 $GIT checkout -f master || exit 4
 $CURL -o script-info.json http://www.vim.org/script-info.php
-if ! $PERL $VAMKR_SAFE_REPO/perl/www_vim_org.pl --verbose
+if ! $PERL $VIMPI_SAFE_REPO/perl/www_vim_org.pl --verbose
 then
     $RM script-info.json
     $GIT reset --hard HEAD
     exit 1
 fi
-if ! $PYTHON -O $VAMKR_SAFE_REPO/python/autoget.py new
+if ! $PYTHON -O $VIMPI_SAFE_REPO/python/autoget.py new
 then
     $RM script-info.json
     $GIT reset --hard HEAD
     exit 1
 fi
 $RM script-info.json
+
+function safe_push()
+{
+    # By the time we are trying to push new changes may have added
+    integer ATTEMPTS=5
+    while ! $GIT push $@ && (( ATTEMPTS-- )) ; do
+        if ! $GIT pull $@
+            return 1
+        fi
+    done
+    (( ATTEMPTS == -1 ))
+    return $?
+}
+
 if $GIT commit ./db -m "Cron job update" ; then
-    $GIT push
+    if safe_push ; then
+        # VAM-kr is a mirror now. Do not try too hard: 5 attempts or any merge 
+        # conflict and “push -f”.
+        safe_push $VAMKRREP || \
+            $GIT push -f $VAMKRREP
+    fi
 fi
