e15a4adfc37c247ef49adfec5d2f222fe1b98cb7
ZyX <kp-pav@ya.ru>
2014-01-20 21:13:48 +0400

    Use more robust checkout

--- zsh/UpdateVAM-kr.zsh
+++ zsh/UpdateVAM-kr.zsh
@@ -13,26 +13,27 @@ RM=/bin/rm
 
 if ! test -d $VIMPIDIR ; then
     test -d $VIMPIDIR:h || /bin/mkdir -p $VIMPIDIR:h || exit 1
-    $GIT clone $VIMPIREP $VIMPIDIR || exit 2
+    $GIT clone --branch master $VIMPIREP $VIMPIDIR || exit 2
     cd $VIMPIDIR || exit 3
 else
     cd $VIMPIDIR || exit 3
     $GIT reset --hard HEAD
-    $GIT pull
+    $GIT checkout --detach HEAD
+    $GIT fetch -f $VIMPIREP master:master || exit 4
 fi
-$GIT checkout -f master || exit 4
+$GIT checkout -f master || exit 5
 $CURL -o script-info.json http://www.vim.org/script-info.php
 if ! $PERL $VIMPI_SAFE_REPO/perl/www_vim_org.pl --verbose
 then
     $RM script-info.json
     $GIT reset --hard HEAD
-    exit 1
+    exit 6
 fi
 if ! $PYTHON -O $VIMPI_SAFE_REPO/python/autoget.py new
 then
     $RM script-info.json
     $GIT reset --hard HEAD
-    exit 1
+    exit 7
 fi
 $RM script-info.json
 
@@ -40,8 +41,8 @@ function safe_push()
 {
     # By the time we are trying to push new changes may have added
     integer ATTEMPTS=5
-    while ! $GIT push $@ && (( ATTEMPTS-- )) ; do
-        if ! $GIT pull $@
+    while ! $GIT push $@ master && (( ATTEMPTS-- )) ; do
+        if ! $GIT pull $@ master
             return 1
         fi
     done
@@ -49,8 +50,8 @@ function safe_push()
     return $?
 }
 
-if $GIT commit ./db -m "Cron job update" ; then
-    if safe_push ; then
+if $GIT commit -a ./db -m "Cron job update" ; then
+    if safe_push $VIMPIREP ; then
         # VAM-kr is a mirror now. Do not try too hard: 5 attempts or any merge 
         # conflict and “push -f”.
         safe_push $VAMKRREP || \
